fd917ddf040f3c4a72a8e9dc2cd28468d2b38e9e
krishtels <karina.krishtafovich@mail.ru>
2023-02-09 20:33:26 +0300

    Add 10000 random rows

--- lab1.sql
+++ lab1.sql
@@ -3,3 +3,18 @@ create table MyTable (
    val NUMBER NOT NULL,
    constraint PK_MYTABLE primary key (id)
 )
+
+DECLARE
+    v NUMBER;
+BEGIN
+    FOR i IN 1..10001
+    LOOP
+       v := DBMS_RANDOM.RANDOM();
+    INSERT INTO 
+        MyTable(id,val)
+    VALUES
+        (i,v);
+    END LOOP;
+END;
+
+SELECT COUNT(*) FROM MyTable
